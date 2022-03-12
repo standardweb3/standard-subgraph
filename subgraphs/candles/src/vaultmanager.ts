5b35a6564804709f4a5b85a519aa2e4b253d7b5e
billjhlee <billjhlee@gmail.com>
2022-03-12 19:16:58 +0900

    dev wip

--- subgraphs/candles/src/vaultmanager.ts
+++ subgraphs/candles/src/vaultmanager.ts
@@ -3,17 +3,14 @@ import { Vault as VaultTemplate } from '../generated/templates'
 import { Address, BigInt } from '@graphprotocol/graph-ts'
 import { Vault } from '../generated/schema'
 
-export function handleNewVault(
-    index: BigInt,
-    collateral: Address,
-): Vault {
-    let vault = new Vault(index.toString())
-    vault.collateral = collateral
-    vault.save()
-    return vault as Vault
+export function handleNewVault(index: BigInt, collateral: Address): Vault {
+  let vault = new Vault(index.toString())
+  vault.collateral = collateral
+  vault.save()
+  return vault as Vault
 }
 
 export function onVaultCreated(event: VaultCreated): void {
-    const vault = handleNewVault(event.params.vaultId, event.params.collateral)
-    VaultTemplate.create(event.params.vault)
+  const vault = handleNewVault(event.params.vaultId, event.params.collateral)
+  VaultTemplate.create(event.params.vault)
 }
